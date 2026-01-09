4b68dc7aa81ae0b224fa241841fc101fb21b56fb
ginodev <luigisirimarco@gmail.com>
2026-01-09 16:40:52 +0100

    Add files via upload

--- test_pytorch_cuda.py
+++ test_pytorch_cuda.py
@@ -2,7 +2,7 @@ import torch
 
 print("Torch version:", torch.__version__)
 print("CUDA version:", torch.version.cuda)
-print("CUDA disponibile:", torch.cuda.is_available())
+print("CUDA is available:", torch.cuda.is_available())
 if torch.cuda.is_available():
-    print("Nome GPU:", torch.cuda.get_device_name(0))
-    print("Numero di GPU:", torch.cuda.device_count())
+    print("GPU Name:", torch.cuda.get_device_name(0))
+    print("GPU Number:", torch.cuda.device_count())
